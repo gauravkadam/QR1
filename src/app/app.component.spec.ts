5ca18bd2d68fc06eb59c8d98a42e92cb2479315b
Nayak <Prakash.Nayak@fiserv.co.in>
2018-08-07 14:08:14 +0530

    intial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,15 +13,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'qr'`, async(() => {
+  it(`should have as title 'demoQR'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('qr');
+    expect(app.title).toEqual('demoQR');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to qr!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to demoQR!');
   }));
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ViewEncapsulation, OnInit } from '@angular/core';
+import { QrScannerComponent } from 'angular2-qrscanner';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
-export class AppComponent {
-  title = 'qr';
+export class AppComponent implements OnInit {
+
+  isCameraOpen: boolean = false;
+  code: string = '';
+
+  @ViewChild(QrScannerComponent) qrScannerComponent: QrScannerComponent;
+
+  ngOnInit() {
+
+  }
+  scan() {
+    if (this.isCameraOpen) {
+      let parent = document.getElementById('cameraArea');
+      let child = document.getElementById('camera');
+      parent.style.display = 'block';
+      this.qrScannerComponent.getMediaDevices().then(devices => {
+        console.log(devices);
+        const videoDevices: MediaDeviceInfo[] = [];
+        for (const device of devices) {
+          if (device.kind.toString() === 'videoinput') {
+            videoDevices.push(device);
+          }
+        }
+        if (videoDevices.length > 0) {
+          let choosenDev;
+          for (const dev of videoDevices) {
+            if (dev.label.includes('front')) {
+              choosenDev = dev;
+              break;
+            }
+          }
+          if (choosenDev) {
+            this.qrScannerComponent.chooseCamera.next(choosenDev);
+          } else {
+            this.qrScannerComponent.chooseCamera.next(videoDevices[0]);
+          }
+        }
+      });
+
+      this.qrScannerComponent.capturedQr.subscribe(result => {
+        console.log(result);
+        this.code = result;
+        this.isCameraOpen = false;
+      });
+    } else {
+      let parent = document.getElementById('cameraArea');
+      let child = document.getElementById('camera');
+      parent.style.display = 'none';
+    }
+  }
 }
